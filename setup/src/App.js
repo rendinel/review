8117bb7f0c3c75156b0637affe878750e119d357
rendinel <rendinel@protonmail.com>
2021-08-16 18:38:59 +0200

    push

---
+++ setup/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Review from './Review'
+// import { FaGithubSquare } from 'react-icons/fa'
+{
+  /* <FaGithubSquare></FaGithubSquare> */
+}
+
+function App() {
+  return (
+    <>
+      <main>
+        <section className='container'>
+          <div className='title'>
+            <h2>Our Reviews</h2>
+            <div className='underline'></div>
+          </div>
+          <Review />
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
